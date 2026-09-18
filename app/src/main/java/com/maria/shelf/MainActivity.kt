6eca35a2541bb79c47b0996068850e22d3fdb792
MariaSultana20 <mariarifa2018@gmail.com>
2026-09-18 23:33:12 +0600

    Week-2: Thurs: Sep17: First Android screen

--- app/src/main/java/com/maria/shelf/MainActivity.kt
+++ app/src/main/java/com/maria/shelf/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,14 +17,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.maria.shelf.ui.theme.ShelfTheme
 
+// Week-2: Thurs: Sep17: First Android screen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val books = listOf(
+            BookWithPages("Power of Your Subconscious Mind", totalPages = 215),
+            BookWithPages("Time Management", totalPages = 128),
+            BookWithPages("Never Stop Learning", totalPages = 140),
+            BookWithPages("The Almanack of Naval Ravikant", totalPages = 239),
+            BookWithPages("Steal Like an Artist", totalPages = 140),
+            BookWithPages("Prottaborton", totalPages = 221),
+            BookWithPages("The Jungle Book", totalPages = 216),
+            BookWithPages("The Kite Runner", totalPages = 324),
+            BookWithPages("A Thousand Splendid Suns", totalPages = 372),
+            BookWithPages("Competitive Programmer's Handbook", totalPages = 285),
+            BookWithPages("Paradoxical Sajid", totalPages = 160),
+            BookWithPages("Paradoxical Sajid 2", totalPages = 225),
+            BookWithPages("Revive Your Heart", totalPages = 160),
+            BookWithPages("Satkahon", totalPages = 728),
+            BookWithPages("The Miracle Morning", totalPages = 304)
+        )
+
         setContent {
             ShelfTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyUI(modifier = Modifier.padding(innerPadding))
+                    ScrollableDataList(dataList = books, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -41,6 +62,18 @@ fun MyUI(modifier: Modifier = Modifier) {
         )
         Button(onClick = {println("Clicked me")}) {
             Text("Click Me")
+        }
+    }
+}
+
+@Composable
+fun ScrollableDataList(dataList: List<BookWithPages>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(dataList) { item ->
+            Text(
+                text = "${item.title}  - ${item.totalPages} pages",
+                modifier = modifier
+            )
         }
     }
 }

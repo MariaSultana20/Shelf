adb3884f9be51a910bbc2376d20f86cbf207b804
MariaSultana20 <mariarifa2018@gmail.com>
2026-09-18 22:52:16 +0600

    Implement basics of UI - Text and button

--- app/src/main/java/com/maria/shelf/MainActivity.kt
+++ app/src/main/java/com/maria/shelf/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +22,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShelfTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MyUI(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShelfTheme {
-        Greeting("Android")
+fun MyUI(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello from Maria!!",
+            modifier = modifier
+        )
+        Text(
+            text = "Sultana",
+            modifier = modifier
+        )
+        Button(onClick = {println("Clicked me")}) {
+            Text("Click Me")
+        }
     }
 }
